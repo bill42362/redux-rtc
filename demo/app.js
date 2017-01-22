caaa3f7cbde4518c72a8baddf3090c0f6ac54323
Bill42362 <bill42362@gmail.com>
2017-01-22 17:00:47 +0800

    Add socket.io server/client to enable peer id syncing.

--- demo/app.js
+++ demo/app.js
@@ -1,20 +1,57 @@
 #!/usr/bin/env node
 'use strict'
 const Express = require('express');
+const Http = require('http');
 
 const App = function() {};
 App.expressStaticRoutes = [
     {path: '/js/', serverPath: '/dist/js'},
     {path: '/', serverPath: '/dist/html/'},
+    {path: '/(:roomId)?', serverPath: '/dist/html/'},
 ];
+App.ioHandlers = [
+    {event: 'connection', handler: function(client) {
+        const app = this;
+        const query = client.handshake.query;
+        const roomId = query.room_id;
+        const clientId = query.client_id;
+        client.join(roomId, function(error) {
+            if(error) { console.log(error); return; }
+            if(!app.rooms[roomId]) { app.rooms[roomId] = []; }
+            app.rooms[roomId].push(clientId);
+            app.io.in(roomId).emit('peerIds', app.rooms[roomId]);
+        }.bind(this));
+        client.on('disconnect', function(reason) {
+            const client = this;
+            const query = client.handshake.query;
+            const roomId = query.room_id;
+            const clientId = query.client_id;
+            const clientIds = app.rooms[roomId];
+            app.rooms[roomId] = clientIds.filter(function(id) {
+                return clientId != id;
+            });
+            app.io.in(roomId).emit('peerIds', app.rooms[roomId]);
+        });
+    }},
+];
+
 App.prototype.server = Express();
+App.prototype.socketServer = Http.createServer(Express());
+App.prototype.rooms = {};
 App.prototype.run = function() {
     const server = this.server;
     App.expressStaticRoutes.forEach(function(route) {
         server.use(route.path, Express.static(__dirname + route.serverPath));
     });
     server.listen(3000);
-    console.log('Listening on 3000 port ...');
+    console.log('Web listening on 3000 port ...');
+
+    this.io = require('socket.io')(this.socketServer).of('/peer');
+    App.ioHandlers.forEach(function(ioHandler) {
+        this.io.on(ioHandler.event, ioHandler.handler.bind(this));
+    }.bind(this));
+    this.socketServer.listen(5566);
+    console.log('Socket listening on 5566 port ...');
 };
 module.exports = App;
 
--- demo/js/App.js
+++ demo/js/App.js
@@ -6,11 +6,26 @@ import ReduxThunk from 'redux-thunk';
 import { connect, Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Core from './Core.js';
+
+import Socket from 'socket.io-client';
+
+const roomId = location.pathname.replace(/\//g, '');
+const clientId = Core.newUuid();
+const io = Socket(`http://localhost:5566/peer?room_id=${roomId}&client_id=${clientId}`);
+const ioCallbacks = [
+    {event: 'connect', callback: () => { console.log('connect()'); }},
+    {event: 'peerIds', callback: (peerIds) => { console.log('peerIds() peerIds:', peerIds); }},
+    {event: 'disconnect', callback: () => { console.log('disconnect()'); }},
+];
+ioCallbacks.forEach(ioCallback => {
+    io.on(ioCallback.event, ioCallback.callback);
+});
 
 const store = createStore(Reducer, applyMiddleware(ReduxThunk));
 var onReadyStateChange = function onReadyStateChange(e) {
     if(document.readyState == 'complete') {
-        store.dispatch(Actions.connectToRemoteId(0.5))
+        store.dispatch(Actions.addPeerAndSetup(0.5))
         .then((peer) => { console.log('peer:', peer, ', state:', store.getState()); });
         ReactDOM.render(
             <Provider store={store} >
